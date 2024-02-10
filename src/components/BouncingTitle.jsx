a1dd3b621f61344671b7ca635e4f69c900ca8d5b
acensia <acensia@snu.ac.kr>
2024-02-11 05:00:59 +0900

    test ended-combining anim

--- src/components/BouncingTitle.jsx
+++ src/components/BouncingTitle.jsx
@@ -1,10 +1,42 @@
 // BouncingLayout.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./BouncingLayout.css"; // Ensure the CSS file is correctly linked
 import harry from "../assets/harry.jpg";
 import hermione from "../assets/hermione.jpg";
 import ron from "../assets/ron.jpg";
 import wand from "../assets/qwand.jpg";
+import style from "styled-components";
+
+const Profile = ({ img }) => {
+  // const [isHovered, setHovered] = useState(false);
+
+  const Styled = style.div`
+    transition: transform 0.4s ease;
+    background-image: url(${img});
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-size: cover;
+    background-position: center 10%;
+    background-repeat: no-repeat;
+    &:hover{
+      transform: scale(1.2);
+    }
+  `;
+
+  // const style = {
+  //   transition: `transform 0.5 ease`,
+  //   // transform: isHovered ? "scale(1.2)" : "scale(1)",
+  //   backgroundImage: `url(${img})`,
+  //   width: "200px",
+  //   height: "200px",
+  //   borderRadius: "50%",
+  //   backgroundSize: "cover" /* Ensures the image covers the entire circle */,
+  //   backgroundPosition: "center 10%" /* Centers the image within the circle */,
+  //   backgroundRepeat: "no-repeat" /* Prevents the image from repeating */,
+  // };
+  return <Styled />;
+};
 
 const BouncingLayout = () => {
   return (
@@ -15,28 +47,34 @@ const BouncingLayout = () => {
       <div className="circles">
         <div
           className="circle pop"
-          style={{ animationDelay: "0.3s", backgroundImage: `url(${harry})` }}
-        ></div>
+          style={{
+            animationDelay: "1.0s",
+          }}
+        >
+          <Profile img={harry} />
+        </div>{" "}
+        {/* <Profile delay="1.0" img={harry} /> */}
         <div
           className="circle pop"
           style={{
-            animationDelay: "0.5s",
-            backgroundImage: `url(${hermione})`,
+            animationDelay: "1.2s",
           }}
-        ></div>
-        <div
-          className="circle pop"
-          style={{ animationDelay: "0.7s", backgroundImage: `url(${ron})` }}
-        ></div>
+        >
+          <Profile img={hermione} />
+        </div>
+        <div className="circle pop" style={{ animationDelay: "1.4s" }}>
+          <Profile img={ron} />
+        </div>
       </div>
       <div
         className="circle pop last"
         style={{
-          animationDelay: "0.9s",
+          animationDelay: "1.6s",
           marginTop: "20px",
-          backgroundImage: `url(${wand})`,
         }}
-      ></div>
+      >
+        <Profile img={wand} />
+      </div>
     </div>
   );
 };
