496551a256b7e10a51d75d2ba1a4b3b605dc526e
acensia <acensia@snu.ac.kr>
2024-02-22 21:29:35 +0900

    fetch code w/ url

--- src/components/Callapi.jsx
+++ src/components/Callapi.jsx
@@ -5,7 +5,14 @@ const callapi = () => {
   //   const rq = new Request("http://172.31.196.7:8080/", {
   //     method: "GET",
   //   });
-  fetch("http://172.31.196.7:8080/").then((res) => console.log(res));
+  fetch(
+    "https://1db11e2a-c024-419f-929d-a7373757d8b1-00-3n8boxj5lw12n.spock.replit.dev/"
+  )
+    .then((response) => response.text()) // Assuming the server responds with plain text
+    .then((data) => {
+      console.log(data); // "Hello World" should be logged here
+    })
+    .catch((error) => console.error("Error fetching data:", error));
   console.log("Passed");
 };
 
