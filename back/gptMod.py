77becc76cbf6e27d38fb7412c5ed37195aedbeec
acensia <acensia@snu.ac.kr>
2024-02-25 22:08:44 +0900

    back code for stduy

--- back/gptMod.py
+++ back/gptMod.py
@@ -3,10 +3,37 @@ from openai import OpenAI
 import os
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+temperature = 1.0
+max_tokens = 512
 
 
 def name(text):
-  return text + "name"
+  inst_inst = f"You are prompt engineer to have gpt make responses better. \
+  Give me an instruction to prompt gpt to respond by mimicking {text}'s \
+  characteristics, including personalities, experiences, speeches, and knowledges. \
+  It includes how friendly with muggles, and how familiar with muggle techknowledges. \
+  Only the instruction sentence, without any explanation."
+
+  default_inst = "You are talking with your muggle friend, but how friendly you are depends on your personality. \
+  If you hate muggles, you don't need to speak friendly. \
+  You can have knowledge for things that appeared \
+  after the Harry Potter era, but how deeply you know them depends on your personality \
+  Match your everything, including the name, experiences, as your characteristic is. \
+  Answer as you are in a dialogue."
+
+  # To summarize, you should tell GPT to act exactly like {messages}. \
+
+  rendered = [{"role": "system", "content": inst_inst}]
+  inst = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=rendered,
+      temperature=temperature,
+      top_p=1.0,
+      max_tokens=max_tokens,
+      stop=["<|endoftext|>"],
+  )
+
+  return inst.choices[0].message.content
 
 
 def check(text):
@@ -14,8 +41,13 @@ def check(text):
 
 
 def sendmsg(text):
-  responses = client.chat.completions.create(model="gpt-3.5-turbo",
-                                             messages=text,
-                                             stream=False)
+  responses = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=text,
+      temperature=temperature,
+      top_p=1.0,
+      max_tokens=max_tokens,
+      stop=["<|endoftext|>"],
+  )
   print(responses.choices[0].message.content)
   return responses.choices[0].message.content
--- back/main.py
+++ back/main.py
@@ -2,12 +2,26 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from keep_alive import keep_alive
-from gptMod import sendmsg
+from gptMod import sendmsg, name
 
 app = Flask(__name__)
 CORS(app)
 
-msg_list = []
+
+class Thr:
+
+  def __init__(self, name=None):
+    self.name = name
+    self.msg_list = []
+
+  def msgAdd(self, msg):
+    self.msg_list.append(msg)
+
+  def instAdd(self, inst):
+    self.msg_list.append({"role": "system", "content": inst})
+
+
+chatbot = Thr()
 
 
 @app.route('/')
@@ -18,20 +32,32 @@ def home():
 @app.route('/polyjuice', methods=['POST'])
 def polyjuice_gpt():
   data = request.json
-  print(data)
+  print(chatbot.name)
   print("Here")
 
   msg = data['text'] if data is not None else None
 
   if msg is None:
     return "Error"
-  msg_list.append({"role": "user", "content": msg})
-  new_msg = sendmsg(msg_list)
-  msg_list.append({"role": "assistant", "content": new_msg})
+  chatbot.msgAdd({"role": "user", "content": msg})
+
+  new_msg = sendmsg(chatbot.msg_list)
+  chatbot.msgAdd({"role": "assistant", "content": new_msg})
 
   return new_msg
 
 
+@app.route('/name', methods=['POST'])
+def name_gpt():
+  data = request.json
+  chatbot.name = data['text'] if data is not None else None
+  inst = name(chatbot.name)
+  chatbot.instAdd(inst)
+  print("name is done")
+
+  return inst
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
-  keep_alive()
+  # keep_alive()
