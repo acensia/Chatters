f3952841e6f127e41f56b779c2a573916a362d95
acensia <acensia@snu.ac.kr>
2024-02-28 18:03:35 +0900

    checker connected

--- src/components/NameSelector.jsx
+++ src/components/NameSelector.jsx
@@ -7,29 +7,45 @@ function MessageBox({ isVisible, onSubmit }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [validationMessage, setValidationMessage] = useState("");
 
-  // Validate the input value before submitting
-  const validateInput = async (value) => {
-    const checked = callapi(value, "/check").then((res) => {
-      console.log(`hi ${res}`);
-      result = res === "yes";
-    });
-    return result;
-  };
+  // // Validate the input value before submitting
+  // const validateInput = async (value) => {
+  //   const checked = callapi(value, "/check").then((res) => {
+  //     console.log(`hi ${res}`);
+  //     result = res === "yes";
+  //   });
+  //   return result;
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validateInput(inputValue)) {
-      setIsSubmitting(true); // Begin the fade-out effect
-      callapi(inputValue, "/name");
-      setTimeout(() => {
-        setIsSubmitting(false);
-        onSubmit(inputValue);
-      }, 1000); // Reset state after animation
-      setValidationMessage(""); // Clear any previous validation messages
-    } else {
-      // Update the state to show a validation message
-      setValidationMessage('Input must include the letter "a".');
-    }
+    const checked = callapi(inputValue, "/check").then((res) => {
+      console.log(`hi ${res}`);
+      if (res !== "no") {
+        setIsSubmitting(true); // Begin the fade-out effect
+        console.log(res);
+        callapi(res, "/name");
+        if (res !== inputValue) setValidationMessage(`Correct name is ${res}`); // Clear any previous validation messages
+        setTimeout(() => {
+          setIsSubmitting(false);
+          onSubmit(res);
+          setValidationMessage(""); // Clear any previous validation messages
+        }, 1000); // Reset state after animation
+      } else {
+        setValidationMessage(`No \"${inputValue}\" in Harry Potter world`);
+      }
+    });
+    // if (validateInput(inputValue)) {
+    //   setIsSubmitting(true); // Begin the fade-out effect
+    //   callapi(inputValue, "/name");
+    //   setTimeout(() => {
+    //     setIsSubmitting(false);
+    //     onSubmit(inputValue);
+    //   }, 1000); // Reset state after animation
+    //   setValidationMessage(""); // Clear any previous validation messages
+    // } else {
+    //   // Update the state to show a validation message
+    //   setValidationMessage('Input must include the letter "a".');
+    // }
   };
 
   if (!isVisible) return null;
